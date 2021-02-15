c1d2a4c195d91c7657d05e4b59070a73fc66bbc2
Arifeen Mahmud <arifeen.mahmud@yahoo.com>
2021-02-15 19:34:02 +0600

    Tax Calculation From User Provided Value.

--- TaxCalculation/main.c
+++ TaxCalculation/main.c
@@ -9,7 +9,21 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+ double income, tax;
+    printf("Enter your Income: ");
+    scanf("%lf", &income);
+    if (income<10000)
+    tax = 0.0;
+    else if (income>=10000 && income<20000)
+    tax = income * .1;
+
+    else if (income>=20000 && income<50000)
+    tax = income * .2;
+
+    else {
+    tax = income *.25;
+    tax = tax + tax * .1;
+    }
+    printf("The payable tax is $%0.2lf \n", tax);
     return 0;
 }
